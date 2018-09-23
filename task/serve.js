1e67c3b721d5ebae789b031bf9436cf6f4f8cb1e
Tekin Tian <tekintian@gmail.com>
2018-09-23 23:14:08 +0800

    serve.js 服务脚本

---
+++ task/serve.js
@@ -0,0 +1,33 @@
+/*
+* @Author: Tekin
+* @Date:   2018-09-23 23:06:40
+* @Last Modified 2018-09-23
+*/
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
+import liveserver from 'gulp-live-server';
+import args from './util/args';
+
+//cb 回调函数名称，可自定义
+gulp.task('serve',(cb)=>{
+	//判断是否处于监听状态，非监听转态直接返回回调函数
+  if(!args.watch) return cb();
+
+// --harmony 表示在当前命令行下执行后面的server/bin/www脚本 [express工具自动创建的]
+  var server = liveserver.new(['--harmony','server/bin/www']);
+  
+  //启动服务器
+  server.start();
+
+//热监听 gulp.watch 做文件监听 数组表示监听的文件路径
+  gulp.watch(['server/public/**/*.js','server/views/**/*.ejs'],function(file){
+    //通知服务器处理
+    server.notify.apply(server,[file]);
+  })
+
+//监听需要重启服务的文件 路由和app.js启动入口文件
+  gulp.watch(['server/routes/**/*.js','server/app.js'],function(){
+    //重启服务器
+    server.start.bind(server)()
+  });
+})
